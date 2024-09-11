0ff87a4cc8417e569ebf8accc73604b56d93e3d0
pychunge <pychungae@connect.ust.hk>
2024-09-11 16:32:31 +0800

    fixed the typo in main and initialize the markdown

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -4,7 +4,7 @@ public class mainApp1 {
     public static void main(String[] args) {
         int b = 2;
         int n = 11;
-        System.out.println("Programing ..Starting...");
+        System.out.println("Program ..Starting...");
         System.out.println("1 + 1 = "+2);
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = " + myLibrary.factorial(n));
